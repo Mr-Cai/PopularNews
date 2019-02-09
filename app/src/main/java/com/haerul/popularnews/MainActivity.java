a0893f2badabbaac01d2b586cb54c245ea99f0a0
aaa <1289181391@qq.com>
2019-02-10 06:07:08 +0800

    更新包

--- app/src/main/java/com/haerul/popularnews/MainActivity.java
+++ app/src/main/java/com/haerul/popularnews/MainActivity.java
@@ -159,34 +159,31 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
 
     private void initListener() {
 
-        adapter.setOnItemClickListener(new Adapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                ImageView imageView = view.findViewById(R.id.img);
-                Intent intent = new Intent(MainActivity.this, NewsDetailActivity.class);
+        adapter.setOnItemClickListener((view, position) -> {
+            ImageView imageView = view.findViewById(R.id.img);
+            Intent intent = new Intent(MainActivity.this, NewsDetailActivity.class);
 
-                Article article = articles.get(position);
-                intent.putExtra("url", article.getUrl());
-                intent.putExtra("title", article.getTitle());
-                intent.putExtra("img", article.getUrlToImage());
-                intent.putExtra("date", article.getPublishedAt());
-                intent.putExtra("source", article.getSource().getName());
-                intent.putExtra("author", article.getAuthor());
+            Article article = articles.get(position);
+            intent.putExtra("url", article.getUrl());
+            intent.putExtra("title", article.getTitle());
+            intent.putExtra("img", article.getUrlToImage());
+            intent.putExtra("date", article.getPublishedAt());
+            intent.putExtra("source", article.getSource().getName());
+            intent.putExtra("author", article.getAuthor());
 
-                Pair<View, String> pair = Pair.create(imageView, ViewCompat.getTransitionName(imageView));
-                ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
-                        MainActivity.this,
-                        pair
-                );
+            Pair<View, String> pair = Pair.create(imageView, ViewCompat.getTransitionName(imageView));
+            ActivityOptionsCompat optionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    MainActivity.this,
+                    pair
+            );
 
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    startActivity(intent, optionsCompat.toBundle());
-                } else {
-                    startActivity(intent);
-                }
-
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                startActivity(intent, optionsCompat.toBundle());
+            } else {
+                startActivity(intent);
             }
+
         });
 
     }
--- app/src/test/java/com/haerul/popularnews/ExampleUnitTest.java
+++ app/src/test/java/com/haerul/popularnews/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.haerul.popularnews;
 
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
